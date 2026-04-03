4cbd204a2659bfcc337455d2045f3a7b0ab6701b
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-04-03 15:01:38 +0300

    added coords check in FlyingRobot

--- app/main.py
+++ app/main.py
@@ -49,6 +49,8 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list | None = None
     ) -> None:
+        if coords is not None and len(coords) < 3:
+            coords.append(0)
         super().__init__(
             name,
             weight,
@@ -79,7 +81,7 @@ class DeliveryDrone(FlyingRobot):
         super().__init__(
             name,
             weight,
-            coords or [0, 0, 0]
+            coords
         )
         self.max_load_weight = max_load_weight
         self.current_load = None
